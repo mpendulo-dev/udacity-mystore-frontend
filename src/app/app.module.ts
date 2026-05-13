eaa23be7f7aabe98b3e119bb86f23b57cd03fec8
Mpendulo <mpendulo.dev@gmail.com>
2026-05-13 20:56:22 +0200

    intial commit and app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    CartComponent,
+    ConfirmationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
